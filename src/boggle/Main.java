6447c1107fe6bddc3cd3d1b2c2765e8ed14e922c
Jarkko Pietilä <jarkko.t.pietila@utu.fi>
2019-04-21 20:35:12 +0300

    Implemented dictionary checking and warning texts

--- src/boggle/Main.java
+++ src/boggle/Main.java
@@ -1,7 +1,6 @@
 package boggle;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -23,6 +22,8 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import javax.swing.text.StyledEditorKit;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -32,13 +33,17 @@ import java.util.Collections;
 
 public class Main extends Application {
 
+    // GUI variables
     private GridPane board;
-    private ArrayList<Tile> pressedTiles = new ArrayList<>();
-    private ObservableList<Word> sanat = FXCollections.observableArrayList();
-    private StringBuilder word = new StringBuilder();
-    private Scoring score = new Scoring();
-    private TableView<Word> table = new TableView();
-    private Label scoreLabel = new Label();
+    private TableView<Word> table;
+    private Label scoreLabel;
+    private Label warningLabel;
+
+    // Game variables
+    private ArrayList<Tile> pressedTiles;
+    private StringBuilder word;
+    private Scoring score;
+    private Dictionary dictionary;
 
     // Initializes the board tiles with dices
     private GridPane initBoard() {
@@ -56,7 +61,6 @@ public class Main extends Application {
         board.setPrefSize(300, 300);
         board.setAlignment(Pos.CENTER_LEFT);
 
-
         // Adding String values (letters) to matrix and Tiles to GridPane
         int k = 0;
         for (int i=0; i < 4; i++) {
@@ -71,19 +75,21 @@ public class Main extends Application {
 
     private TableView createTable() {
         // Create a table to show the words
+        table = new TableView<>();
         table.setEditable(true);
         TableColumn<Word, String> sanaCol = new TableColumn("Words");
         sanaCol.setCellValueFactory(new PropertyValueFactory<Word, String>("wordToAdd"));
         table.setColumnResizePolicy(table.CONSTRAINED_RESIZE_POLICY);
         table.getColumns().add(sanaCol);
         table.setTranslateX(320);
-
         return table;
     }
 
-    private VBox scoreLabel() {
+    // Label to show the current word that the user is trying to get
+    private VBox createScoreLabel() {
         Label pisteet = new Label("Score:");
         pisteet.setFont(new Font("Arial", 30));
+        scoreLabel = new Label();
         scoreLabel.setText("0");
         scoreLabel.setFont(new Font("Arial", 32));
         final VBox vbox = new VBox();
@@ -92,11 +98,23 @@ public class Main extends Application {
         vbox.getChildren().addAll(pisteet, scoreLabel);
         vbox.setTranslateX(200);
         vbox.setTranslateY(50);
-
         return vbox;
     }
 
-    // Label to show the current word that the user is trying to get
+    private VBox createWarningLabel() { ;
+        warningLabel = new Label();
+        warningLabel.setText("");
+        warningLabel.setTextFill(Color.RED);
+        warningLabel.setFont(new Font("Arial", 13));
+        final VBox vbox = new VBox();
+        vbox.setSpacing(5);
+        vbox.setPadding(new Insets(10, 0, 0, 10));
+        vbox.getChildren().add(warningLabel);
+        vbox.setTranslateX(0);
+        vbox.setTranslateY(250);
+        return vbox;
+    }
+
 
     // Populates the window with nodes
     private Parent createRoot() {
@@ -104,7 +122,8 @@ public class Main extends Application {
         root.setPrefSize(570, 450);
         root.getChildren().addAll(initBoard());
         root.getChildren().addAll(createTable());
-        root.getChildren().addAll(scoreLabel());
+        root.getChildren().addAll(createScoreLabel());
+        root.getChildren().add(createWarningLabel());
         Button reset = new Button("Reset");
         reset.setTranslateX(210);
         reset.setTranslateY(180);
@@ -122,14 +141,19 @@ public class Main extends Application {
     }
 
     private void confirm() {
-        //if (word.isValidWord && not already used) {
-        String w = word.toString();
-        Word addedWord = new Word(w);
-        table.getItems().add(addedWord);
+        String w = word.toString().toLowerCase();
+        if (dictionary.contains(w)){
+            Word addedWord = new Word(w);
+            table.getItems().add(addedWord);
             score.countScore(word);
             System.out.println(score.getScore());
-        scoreLabel.setText("" + score.getScore());
-        //}
+            scoreLabel.setText("" + score.getScore());
+            reset();
+            warningLabel.setText("");
+        } else {
+            reset();
+            warningLabel.setText("Sanaa ei löytynyt sanakirjasta.");
+        }
     }
 
     // Rolls dice in the start of a new round
@@ -210,6 +234,18 @@ public class Main extends Application {
         primaryStage.show();
     }
 
+    @Override
+    public void init() {
+        pressedTiles = new ArrayList<>();
+        word = new StringBuilder();
+        score = new Scoring();
+        try {
+            dictionary = new Dictionary();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
     /**
      * Class representing a single tile on the board
      */
@@ -256,6 +292,7 @@ public class Main extends Application {
 
                 }
             } else {
+                warningLabel.setText("Saman ruudun painaminen aloittaa sanan alusta.");
                 reset();
             }
         }
